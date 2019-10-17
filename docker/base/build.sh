75a680a91ca8521252c2c3d48baa89d5fd78242b
CrazyMax <crazy-max@users.noreply.github.com>
2019-10-18 01:16:26 +0200

    Go 1.12.12, 1.13.3

--- docker/base/build.sh
+++ docker/base/build.sh
@@ -147,9 +147,13 @@ USR_LOCAL_CONTENTS=`ls /usr/local`
 # Configure some global build parameters
 NAME=`basename $1/$PACK`
 
-# Extract name from module name
+# Go module-based builds error with 'cannot find main module'
+# when $PACK is defined
 if [[ "$USEMODULES" = true ]]; then
+  PACK_RELPATH=""
   NAME=`sed -n 's/module\ \(.*\)/\1/p' /source/go.mod`
+else
+  PACK_RELPATH="./$PACK"
 fi
 
 # Pack relative path
