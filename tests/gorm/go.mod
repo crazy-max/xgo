15b682140027a511734b09cb6fede7d26a7cc0e2
CrazyMax <crazy-max@users.noreply.github.com>
2021-03-12 14:15:51 +0100

    Add gorm test

---
+++ tests/gorm/go.mod
@@ -0,0 +1,5 @@
+module github.com/crazy-max/xgo/test/gorm
+
+go 1.16
+
+require github.com/jinzhu/gorm v1.9.16
---
+++ tests/gorm/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+)
+
+type Product struct {
+	gorm.Model
+	Code  string
+	Price uint
+}
+
+func main() {
+	db, err := gorm.Open("sqlite3", "test.db")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	defer db.Close()
+
+	// Migrate the schema
+	db.AutoMigrate(&Product{})
+
+	// Create
+	db.Create(&Product{Code: "L1212", Price: 1000})
+
+	// Read
+	var product Product
+	db.First(&product, 1)                   // find product with id 1
+	db.First(&product, "code = ?", "L1212") // find product with code l1212
+
+	// Update - update product's price to 2000
+	db.Model(&product).Update("Price", 2000)
+
+	// Delete - delete product
+	db.Delete(&product)
+}
